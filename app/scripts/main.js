44409b9f946e1e9d0b8effe67d141a092e2019d2
Edwin Espaderos <eespaderos@gmail.com>
2014-01-23 04:54:05 -0500

    added all ther objects

---
+++ app/scripts/main.js
@@ -0,0 +1,40 @@
+console.log('El Edwin');
+
+var wolf = {
+	name: "Juan",
+	legs: 4,
+	ears: 2,
+	tail: 1,
+	food: ["fish", "rabbit", "hiker", "jogger"],
+	eating: function(){
+		
+	}
+};
+ 
+var hiker = {
+	 name: "Dan",	 
+	 legs: 2,
+	 hat: 1,
+	 boots: true,
+	 food: false,
+	 backpack: ["flashlight", "map", "rope", "food"]
+	 hiking: function(){
+
+	 }
+
+};
+
+var jogger = {
+	name: "Jane",
+	legs: 2,
+	shoes: true,
+	fannypack: ["ipod", "snack", "wallet"]
+	food: false,
+	running: function(){
+
+	}
+};
+
+// countEars: function(){
+// 	console.log('I have' this.ears 'On my back')
+// };
---
+++ app/scripts/new.js
@@ -0,0 +1,16 @@
+
+
+
+var wolf = {
+	legs: 4,
+	ears: 2,
+	name: "Juan"
+	tail: 1,
+	teeth: 24,
+	bark: function(){
+		console.log('WOOF!')
+	}
+},
+countEars: function(){
+	console.log('I have' this.ears 'On my back')
+};
